51b839d9bb14620567adcd33824ce6250703dfea
Andrew Thumma <andrewthumma@gmail.com>
2023-06-15 13:08:25 -0400

    qualifiers

--- src/main/java/guru/springframework/spring6webapp/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/PropertyInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6webapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.spring6webapp.services.GreetingService;
@@ -8,6 +9,7 @@ import guru.springframework.spring6webapp.services.GreetingService;
 @Controller
 public class PropertyInjectedController {
     
+    @Qualifier("greetingServicePropertyInjected")
     @Autowired
     GreetingService greetingService;
 
---
+++ src/main/java/guru/springframework/spring6webapp/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6webapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServicePropertyInjected implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Friends don't let friends do property injection!";
+    }
+    
+}
