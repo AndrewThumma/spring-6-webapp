3b6e103596f768690ccacbaae600ab24133564f8
Andrew Thumma <andrewthumma@gmail.com>
2023-06-27 09:18:38 -0400

    Lecture 111

--- src/test/java/guru/springframework/spring6webapp/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6webapp/controllers/BeerControllerTest.java
@@ -134,4 +134,17 @@ class BeerControllerTest {
 
         assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
     }
+
+    @Test
+    void testCreateBeerNullBeerName() throws Exception {
+        BeerDTO dto = BeerDTO.builder().build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mvc.perform(post(BeerController.BEER_PATH)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(dto)))
+            .andExpect(status().isBadRequest());
+    }
 }
