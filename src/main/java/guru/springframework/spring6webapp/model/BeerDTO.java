552303507fffadd89e4212cd2980e916c04534b6
Andrew Thumma <andrewthumma@gmail.com>
2023-06-29 12:00:38 -0400

    cleanup

--- src/main/java/guru/springframework/spring6webapp/model/BeerDTO.java
+++ src/main/java/guru/springframework/spring6webapp/model/BeerDTO.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
